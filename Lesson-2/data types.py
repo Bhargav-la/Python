81f629a74a1c275a2efc72696f8e1ab7e550ac25
Bhargav-la <er.bhargavkashyap8@gmail.com>
2025-06-10 19:27:15 +0530

    table

---
+++ Lesson-2/data types.py
@@ -0,0 +1,8 @@
+name = 'Bhargav'
+age = 10
+is_student = True
+weight = 18.60
+print("Name is", name,"The type is ",type(name))
+print("Age is ", age,"The type is ",type(age))
+print("Is_student is", is_student,"The type is",type(is_student))
+print("Weight is ", weight,"The type is ",type(weight))
